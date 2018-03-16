4c1dbd9e3066edd811ee267e9c84ae30064a20dd
shubhamarya9 <shubhamarya9@gmail.com>
2018-03-15 21:40:44 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function openNav() {
+	console.log('123');
+    document.getElementById("mySidenav").style.width = "200px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+function openNav1() {
+	console.log('123');
+    document.getElementById("mySidenav1").style.width = "200px";
+}
+
+function closeNav1() {
+    document.getElementById("mySidenav1").style.width = "0";
+}
